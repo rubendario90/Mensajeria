2061f24eda22fd86928c4826288ddd4bfcf1592b
Rbayonagalvis <rbayonagalvis@gmail.com>
2023-07-10 22:31:01 -0500

    cambios

--- php/index.php
+++ php/index.php
@@ -7,29 +7,57 @@
   <title>Página con Bootstrap</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="https://unpkg.com/transition-style">
+  <style>
+    body,
+    html {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+    }
 
-  <head>
-    
-        
-  
+    .video-container {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      overflow: hidden;
+    }
+
+    video {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  </style>
+</head>
 
 <body>
-<div class="px-3 py-2 text-bg-primary border-bottom">
-      <div class="container">
-
-        <div class="text-end">
-        <a href="login.php"><button type="button" class="btn btn-outline-light me-2">Ingresar</button></a>
-        <a href="registro.php"><button type="button" class="btn btn-outline-light me-2">Registrarse</button></a>
-      </div>
-    </div>
-<div class="px-3 py-2 border-bottom mb-3">
-  <div class="container">
-    <div class="text-center">
-      <div transition-style="in:wipe:up">
-        <h1 class="text-white">Bienvenidos</h1>
+  <div class="px-3 py-2  border-bottom">
+    <div class="container">
+      <div class="text-end">
+        <a href="login.php"><button type="button" class="btn btn-outline-dark me-2 custom-btn">Ingresar</button></a>
+        <a href="registro.php"><button type="button" class="btn btn-outline-dark me-2 custom-btn">Registrarse</button></a>
+        <style>
+          .custom-btn {
+            background-color: #F2BC1B;
+            color: grey;
+          }
+        </style>
       </div>
     </div>
   </div>
+
+
+  <div class="video-container">
+    <video autoplay muted loop>
+      <source src="images/logo.mp4" type="video/mp4">
+    </video>
+  </div>
+
+
+
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 </body>
